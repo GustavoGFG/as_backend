f0f47b99f8201459423718775130b5cc48d6e71d
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-03-27 09:21:39 -0300

    :recycle:Refactor: server.listen + vercel.json

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import https from 'https';
-import http from 'http';
-import fs from 'fs'
+import http, { Server } from 'http';
+import fs from 'fs';
 
 import siteRoutes from './routes/site';
 import adminRoutes from './routes/admin';
@@ -24,27 +24,36 @@ app.all('*', requestIntereptor);
 app.use('/admin', adminRoutes);
 app.use('/', siteRoutes);
 
-// LISTEN TO SERVER
-const runServer = (port: number, server: http.Server) => {
-  server.listen(port, () => {
-    console.log(`Running at PORT ${port}`);
-  });
-};
-
-const regularServer = http.createServer(app);
-if (process.env.NODE_ENV === 'production') {
-  //TODO: config SSL
-  const options = {
-    key: fs.readFileSync(process.env.SSL_KEY as string),
-    cert: fs.readFileSync(process.env.SSL_CERT as string)
+// LISTEN TO SERVER 1.0
+app.listen(process.env.PORT || 3000, (error?: Error) => {
+  if (!error) {
+    console.log('Server Running on Port 3000');
+  } else {
+    console.log('Error: ', error);
   }
-  // TODO: rodar server na 80 e na 443
-  const secServer = https.createServer(options, app)
-  runServer(80, regularServer)
-  runServer(443, secServer)
-} else {
-  const serverPort: number = process.env.PORT
-    ? parseInt(process.env.PORT)
-    : 9000;
-  runServer(serverPort, regularServer);
-}
+});
+
+// LISTEN TO SERVER
+// const runServer = (port: number, server: http.Server) => {
+//   server.listen(port, () => {
+//     console.log(`Running at PORT ${port}`);
+//   });
+// };
+
+// const regularServer = http.createServer(app);
+// if (process.env.NODE_ENV === 'production') {
+//   //TODO: config SSL
+//   const options = {
+//     key: fs.readFileSync(process.env.SSL_KEY as string),
+//     cert: fs.readFileSync(process.env.SSL_CERT as string)
+//   }
+//   // TODO: rodar server na 80 e na 443
+//   const secServer = https.createServer(options, app)
+//   runServer(80, regularServer)
+//   runServer(443, secServer)
+// } else {
+//   const serverPort: number = process.env.PORT
+//     ? parseInt(process.env.PORT)
+//     : 9000;
+//   runServer(serverPort, regularServer);
+// }
